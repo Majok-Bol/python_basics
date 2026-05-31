c4088a989f1ea9e1d41f1eeadf9b1b4be72ddb38
Majok-Bol <bolmajok001@gmail.com>
2026-05-31 17:34:35 +0300

    added method overriding in python classes

--- classes/classes.py
+++ classes/classes.py
@@ -1,8 +1,10 @@
 class User:
     def __init__(self,username,email):
+        #self refers to the current object.
         self.username=username
         self.email=email
-        #function to print username and email
+    #function to print username and email
+    #Methods are simply functions inside a class.
     def display_info(self):
         print(f"\nUsername: {self.username}")
         print(f"\nEmail: {self.email}")
@@ -14,3 +16,63 @@ user1.display_info()
 user2=User("alex","alex@gmail.com")
 #display results
 user2.display_info()
+
+
+
+#Inheritance
+#means create a new class using an existing class
+#example
+#create another class Admin
+#which inherits from User class
+class Admin(User):
+    #extend parent's functionality
+    def __init__(self,username,email,role):
+        super().__init__(username,email)
+        self.role=role
+    #function to display details
+    def display_details(self):
+        print(f"\nUsername: {self.username}\nEmail: {self.email}\nRole: {self.role}")
+    
+
+
+#create an instance of Admin class
+admin1=Admin(
+    "Bashbytes Imla",
+    "bashbytesimla@gmail.com",
+    "Super admin"
+)
+#show results
+admin1.display_details()
+
+
+
+#method overriding
+#happens when a child class provide its own versio of a method that already exist in the parent class
+#example
+class User2:
+    def login(self):
+        print("\nLogin successful")
+class Admin2(User2):
+    def login(self):
+        print("\nLogin successful")
+        print("Opening admin panel")
+class Moderator(User2):
+    def login(self):
+        print("\nLogin successful...")
+        print("Opening moderation tools\n")
+
+
+#instance of user1
+user_one=User2()
+#call login method
+user_one.login()
+
+#instance of admin 2
+admin_two=Admin2()
+#call login method
+admin_two.login()
+
+#instance of Moderator
+moderator=Moderator()
+#call login method
+moderator.login()
